746a08008e3a1ec6a9e586675caf34f09a0703c3
Alphabot15 <92548620+Alphabot15@users.noreply.github.com>
2022-04-21 18:35:55 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "Todavia miente";
+const accessCode2 = "Soborno";
+const accessCode3 = "Justicia";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("ACERTIJO 1", 100,50)
+    fill("lightblue")
+    text("¿Qué hace un mentiroso cuando está muerto?", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("ACERTIJO 2", 700,150)
+    fill("lightblue")
+    text("Cuál es el precio de tu ojo ciego?", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("ACERTIJO 3", 100,250)
+    fill("lightblue")
+    text("Puede ser cruel, poético o ciego. Pero cuando se niega, es violencia lo que puedes encontrar.  ", 100,280)
+    
+
+}
